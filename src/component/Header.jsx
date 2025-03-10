9508907b36bf8e8457fa58594343a07467fc3b1a
skSadi12 <sksadi433@gmail.com>
2025-03-10 16:28:33 +0600

    header section done

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-5xl mt-5 mb-5'>Balance Tracker</h1>
+    </div>
+  )
+}
+
+export default Header
